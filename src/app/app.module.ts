8e439e3ca5d18d34493b9837fc920dbfaad68409
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-14 04:14:01 +0200

    chore: add support for local search (still prototyping)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from '@app/core/material.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MaterialModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MaterialModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/search-ui/search.service.ts
+++ src/app/search-ui/search.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+import { environment } from '@env/environment';
+
+export interface Application {
+  category: string;
+  rating: number;
+  name: string;
+  image: string;
+  link: string;
+  ratingCount: number;
+  price: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  fetchMocks(search: string) {
+    return this.http.get(environment.mock.url).pipe(
+      map((data: Application[]) => {
+        return data.map(d => {
+          d.image = 'https://ng2training.com/wp-content/uploads/2015/12/angular.png';
+          return d;
+        });
+      }),
+      map(apps => apps.filter(app => search.length > 0 && new RegExp(`${search}`).test(app.name)))
+    );
+  }
 }
