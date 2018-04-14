a90c7689d3bd1de277392d0128e359f34826b3d3
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-15 00:50:10 +0200

    chore(server): add initial function implementation

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,44 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cookieParser = require('cookie-parser')();
+const cors = require('cors')({ origin: true });
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const FAKE_TOKEN = 'this-is-a-fake-token';
+const BEARER = 'SearchToken';
+
+const validateAuthorizedToken = (req, res, next) => {
+  console.log('Check if request is authorized with the correct Agent');
+
+  if ((!req.headers.authorization || !req.headers.authorization.startsWith(`${BEARER} `)) && !req.cookies.__session) {
+    console.error('No valid token was passed as a Bearer token in the Authorization header.');
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  if (req.headers.authorization && req.headers.authorization.startsWith(`${BEARER} `)) {
+    console.log('Found "Authorization" header');
+    const idToken = req.headers.authorization.split(`${BEARER} `)[1];
+
+    // accept any token (for the sake of this demo app)
+    if (idToken === FAKE_TOKEN) {
+      return next();
+    }
+
+  }
+
+  res.status(403).send('Unauthorized');
+  return;
+};
+
+app.use(cors);
+app.use(cookieParser);
+app.use(validateAuthorizedToken);
+app.post('/api/1/apps', (req, res) => {
+  res.send(`Added ${req.body}`);
+});
+app.delete('/api/1/apps/:id', (req, res) => {
+  res.send(`Deleting ${req.id}`);
+});
+
+exports.search = functions.https.onRequest(app);
