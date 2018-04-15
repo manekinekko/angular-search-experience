f87c84061f2bf217f5d2770edee7e1e6bd7f3fa7
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-15 23:57:13 +0200

    chore: rework the indeces and replicas (necessary for sorting strategies)

--- src/app/core/algolia/algolia.service.ts
+++ src/app/core/algolia/algolia.service.ts
@@ -3,80 +3,95 @@ import { Injectable, Inject } from '@angular/core';
 import algoliasearchHelper from 'algoliasearch-helper';
 import { ALGOLIA_APPLICATION_ID, ALGOLIA_SEARCH_API_KEY, ALGOLIA_INDEX } from './injection-tokens';
 
+export interface SearchState {
+  search$: Observable<any>;
+  result$: Observable<any>;
+  change$: Observable<any>;
+  error$: Observable<any>;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AlgoliaService {
-  helper;
-  searchState$: Observable<any>;
-  resultState$: Observable<any>;
-  changeState$: Observable<any>;
-  errorState$: Observable<any>;
+  indices = {
+    applications: algoliasearchHelper,
+    applications_by_rating_desc: algoliasearchHelper,
+    applications_by_price_asc: algoliasearchHelper
+  };
+  client;
+
+  searchState: SearchState;
 
   constructor(
     @Inject(ALGOLIA_APPLICATION_ID) private applicationID,
     @Inject(ALGOLIA_SEARCH_API_KEY) private searchApiKey,
     @Inject(ALGOLIA_INDEX) private indexName
   ) {
-    const client = window['algoliasearch'](applicationID, searchApiKey);
-    this.helper = algoliasearchHelper(client, indexName, {
+    this.client = window['algoliasearch'](applicationID, searchApiKey);
+
+    this.searchState = {} as any;
+
+    this.configureIndex('applications');
+  }
+
+  configureIndex(indexName: string) {
+    this.indices[indexName] = algoliasearchHelper(this.client, indexName, {
       disjunctiveFacets: ['category']
     });
 
-    // handle search events
-    this.searchState$ = new Observable(observer => {
-      const handler = e => observer.next(e);
-      this.helper.on('search', handler);
-      return () => {
-        this.helper.removeListener('search', handler);
-      };
-    });
+    this.searchState = {} as SearchState;
 
-    // handle result events
-    this.resultState$ = new Observable(observer => {
-      const handler = e => observer.next(e);
-      this.helper.on('result', handler);
-      return () => {
-        this.helper.removeListener('result', handler);
-      };
-    });
-
-    // handle change events
-    this.changeState$ = new Observable(observer => {
-      const handler = e => observer.next(e);
-      this.helper.on('change', handler);
-      return () => {
-        this.helper.removeListener('change', handler);
-      };
-    });
-
-    // handle error events
-    this.errorState$ = new Observable(observer => {
-      const handler = e => observer.next(e);
-      this.helper.on('error', handler);
-      return () => {
-        this.helper.removeListener('error', handler);
-      };
+    // handle algolia events
+    ['search', 'result', 'change', 'error'].forEach(eventName => {
+      this.searchState[`${eventName}$`] = new Observable(observer => {
+        const handler = e => observer.next(e);
+        this.indices[indexName].on(eventName, handler);
+        return () => this.indices[indexName].removeListener(eventName, handler);
+      });
     });
   }
 
   configure(options) {
-    this.helper.setQueryParameter(options);
+    this.indices.applications.setQueryParameter(options);
   }
 
-  search(query: string) {
-    if (query) {
-      this.helper.setQuery(query).search();
+  private search(indexName: string, query: string) {
+    if (query.trim() !== '') {
+      console.log(this.indices.applications.getIndex());
+      this.indices.applications
+        .setIndex(indexName)
+        .setQuery(query)
+        .search(error => {
+          console.log(error.message);
+          console.log(error.debugData);
+          return;
+        });
     }
   }
 
   toggleFacetRefinement(facet: any = {}) {
-    this.helper = this.helper.toggleFacetRefinement(facet.name, facet.query).search();
+    this.indices.applications.toggleFacetRefinement(facet.name, facet.query).search();
   }
 
   nextPage() {
-    if (this.helper.state.query !== '') {
-      this.helper.nextPage().search();
+    if (this.indices.applications.state.query !== '') {
+      console.log(this.indices.applications.getIndex());
+
+      this.indices.applications.nextPage().search();
     }
+  }
+
+  sortByPriceAsc(query?: string) {
+    query = query || this.indices.applications.state.query;
+    this.search('applications_by_price_asc', query);
+  }
+  sortByRatingDesc(query?: string) {
+    query = query || this.indices.applications.state.query;
+    this.search('applications_by_rating_desc', query);
+  }
+  sortByRelevance(query?: string) {
+    query = query || this.indices.applications.state.query;
+    this.search('applications', query);
   }
 }
