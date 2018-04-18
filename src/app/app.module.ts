470294a6b36d55025c297b4bbd9fed02979de990
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-18 21:21:12 +0200

    chore: add ssr support (wip)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
-import { CoreModule } from '@app/core/core.module';
+import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, CoreModule, AppRoutingModule],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'angular-search-experience' }),
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    AppRoutingModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
