78c7eb832579b3b48c293468e511f0034459c129
Wassim CHEGHAM <github@wassimchegham.com>
2018-04-20 01:13:07 +0200

    feat: add pwa'ness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from '@app/core/core.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, CoreModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
