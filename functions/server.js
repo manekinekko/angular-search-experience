db252541ac38b12b3e3845a8339e4dd901853a4d
Wolfgang Goedel <wgoedel@gmail.com>
2018-10-25 15:53:31 +0200

    fix(devServer): log correct port when set in env

--- functions/server.js
+++ functions/server.js
@@ -1,7 +1,8 @@
 const server = require('./src/search').app;
+const port = process.env.PORT || 5000;
 
-server.listen(process.env.PORT || 5000, '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:5000`);
-  console.log(`- POST: http://localhost:5000/1/apps/`);
-  console.log(`- DELETE: http://localhost:5000/1/apps/:id`);
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`- POST: http://localhost:${port}/1/apps/`);
+  console.log(`- DELETE: http://localhost:${port}/1/apps/:id`);
 });
